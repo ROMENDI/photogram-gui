a2e3a7d2982bf0870a11a0f758cdf15941c8e8b3
Rohan Mendiratta <97857962+ROMENDI@users.noreply.github.com>
2024-02-14 22:14:45 +0000

    added delete photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,11 +3,23 @@ class PhotosController < ApplicationController
   def index 
     matching_photos = Photo.all 
     @list_of_photos = matching_photos.order({ :created_at => :desc})
-    render({:template => "/photo_templates/index"})
+    render({ :template => "/photo_templates/index"})
   end 
 
   def show
-    render({:template => "/photo_templates/show"})
+    url_id = params.fetch("path_id")
+    matching_photos= Photo.where({:id => url_id})
+    @the_photo = matching_photos.first
+    render({ :template => "/photo_templates/show"})
   end 
+
+  def destroy 
+    the_id= params.fetch("path_id")
+    matching_photos = Photo.where({:id => the_id})
+    the_photo = matching_photos.first 
+    the_photo.destroy
+    redirect_to("/photos", { :notice => "Photo deleted successfully."})
+  end 
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,7 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:path_username", { :controller => "users", :action => "show" })
   get("/photos", { :controller => "photos", :action => "index" })
-  
-
-
-
-
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
 
 end
